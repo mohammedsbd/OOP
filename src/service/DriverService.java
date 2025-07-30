3e4c025afe1a49b32391a72a0a986f9186a3e0c7
mohammedsbd <mohammed1112ok@gmail.com>
2025-07-30 10:33:36 -0700

    add driver service

--- src/service/DriverService.java
+++ src/service/DriverService.java
@@ -1,44 +1,37 @@
 package service;
 
-import model.Customer;
-import dao.CustomerDAO;
-import util.InputValidator;
+import model.Driver;
+import dao.DriverDAO;
+import java.util.List;
 
-public class AuthService {
-    private CustomerDAO customerDAO;
+public class DriverService {
+    private DriverDAO driverDAO;
     
-    public AuthService() {
-        this.customerDAO = new CustomerDAO();
+    public DriverService() {
+        this.driverDAO = new DriverDAO();
     }
     
-    public Customer registerCustomer(String name, String email, String phone, String password) {
-        if (!InputValidator.isValidEmail(email)) {
-            System.out.println("Invalid email format");
-            return null;
-        }
-        
-        if (!InputValidator.isValidPhone(phone)) {
-            System.out.println("Invalid phone number");
-            return null;
-        }
-        
-        if (customerDAO.getCustomerByEmail(email) != null) {
-            System.out.println("Email already registered");
-            return null;
-        }
-        
-        Customer customer = new Customer(0, name, phone, email, password);
-        if (customerDAO.addCustomer(customer)) {
-            return customerDAO.getCustomerByEmail(email);
-        }
-        return null;
+    public boolean addDriver(Driver driver) {
+        return driverDAO.addDriver(driver);
     }
     
-    public Customer login(String email, String password) {
-        Customer customer = customerDAO.getCustomerByEmail(email);
-        if (customer != null && customer.getPassword().equals(password)) {
-            return customer;
-        }
-        return null;
+    public boolean updateDriver(Driver driver) {
+        return driverDAO.updateDriver(driver);
+    }
+    
+    public boolean deleteDriver(int driverId) {
+        return driverDAO.deleteDriver(driverId);
+    }
+    
+    public Driver getDriverById(int id) {
+        return driverDAO.getDriverById(id);
+    }
+    
+    public List<Driver> getAvailableDrivers() {
+        return driverDAO.getAvailableDrivers();
+    }
+    
+    public List<Driver> getAllDrivers() {
+        return driverDAO.getAllDrivers();
     }
 }
