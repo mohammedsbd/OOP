6ed94f57837848fa62469e0c2725aacb75eec2c7
mohammedsbd <mohammed1112ok@gmail.com>
2025-07-30 09:05:26 -0700

    add abstract class and logger java class

--- src/util/Logger.java
+++ src/util/Logger.java
@@ -1,5 +1,21 @@
 package util;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Logger {
-    
+    private static final String LOG_FILE = "app_logs.txt";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public static void log(String message) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOG_FILE, true))) {
+            String timestamp = LocalDateTime.now().format(formatter);
+            writer.write(String.format("[%s] %s%n", timestamp, message));
+        } catch (IOException e) {
+            System.err.println("Error writing to log file: " + e.getMessage());
+        }
+    }
 }
